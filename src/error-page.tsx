f9cc8a8ee4e63c8917da4fedb440e983fb9cf522
Achille <chillo.services@gmail.com>
2024-09-29 12:30:26 +0200

    Configuration navigation

---
+++ src/error-page.tsx
@@ -0,0 +1,20 @@
+import { useRouteError } from "react-router-dom";
+type ErrorType = {
+  statusText?: string;
+  message?: string;
+};
+function ErrorPage() {
+  const error = useRouteError() as ErrorType;
+  console.error(error);
+
+  return (
+    <div>
+      <h1>La page que vous recherchez n'existe pas</h1>
+      <p>
+        <i>{error?.statusText || error?.message}</i>
+      </p>
+    </div>
+  );
+}
+
+export default ErrorPage;
---
+++ src/pages/Declarations.tsx
@@ -0,0 +1,5 @@
+function Declarations() {
+  return <div className="border border-blue-200">Declarations</div>;
+}
+
+export default Declarations;
