89e7a57dd5256e709c93216f40445e30c369f674
Achille <chillo.services@gmail.com>
2024-09-29 13:40:00 +0200

    Configuration du layout

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { NAV_LINKS } from "../../utils/data";
+
+function Nav() {
+  return (
+    <nav className="z-10 bg-white shadow-md w-56 fixed flex flex-col justify-between top-0 left-0 bottom-0">
+      <Link
+        to={"/private/declarations"}
+        className="bg-blue-700 text-white uppercase text-center py-5 font-extrabold text-xl"
+      >
+        les nouvelles vies
+      </Link>
+      <ul>
+        {NAV_LINKS.map(({ to, label }, index) => (
+          <li
+            key={`navlink-${index}`}
+            className={`border-b border-gray-300 ${
+              index === 0 ? "border-t" : null
+            }`}
+          >
+            <Link to={to} className="py-2 pl-2 hover:bg-gray-100 block">
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <button
+        type="button"
+        onClick={() => null}
+        className="bg-red-700 text-white hover:text-red-700 py-2 font-normal hover:border hover:border-red-700 hover:bg-white"
+      >
+        Déconnexion
+      </button>
+    </nav>
+  );
+}
+
+export default Nav;
