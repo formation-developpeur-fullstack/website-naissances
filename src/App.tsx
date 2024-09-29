da14e51f0441876447e78cd81efc622e2c43c394
Achille <chillo.services@gmail.com>
2024-09-29 10:41:29 +0200

    Configuration tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <main>Gestion de naissances</main>;
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Gestion de naissances</h1>
+    </main>
+  );
 }
 
 export default App;
